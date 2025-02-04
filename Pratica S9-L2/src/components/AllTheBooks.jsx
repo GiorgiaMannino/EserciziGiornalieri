3964d238e99cdeff6f5aa8b0439c68185bc4b905
GiorgiaMannino <giorgimannino@hotmail.com>
2025-02-04 18:23:13 +0100

    Pratica S9 L2

--- Pratica S9-L2/src/components/AllTheBooks.jsx
+++ Pratica S9-L2/src/components/AllTheBooks.jsx
@@ -1,4 +1,4 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import fantasy from "../fantasy.json";
 
 const AllTheBooks = () => {
@@ -8,12 +8,22 @@ const AllTheBooks = () => {
         {fantasy.map((book) => {
           return (
             <Col xs={12} sm={6} md={3} lg={2} key={book.asin}>
-              <Card className="mb-5 border-0 text-center shadow Card">
-                <Card.Img variant="top" src={book.img} />
+              <Card className="mb-5 border-0 text-center shadow Card " style={{ width: "100%", height: "90%" }}>
+                <Card.Img
+                  variant="top"
+                  src={book.img}
+                  style={{
+                    height: "250px",
+                    width: "100%",
+                  }}
+                />
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <Card.Text>€ {book.price}</Card.Text>
                 </Card.Body>
+                <Button variant="primary" className="mb-3 w-50 ms-3">
+                  Acquista
+                </Button>
               </Card>
             </Col>
           );
@@ -22,4 +32,5 @@ const AllTheBooks = () => {
     </Container>
   );
 };
+
 export default AllTheBooks;
