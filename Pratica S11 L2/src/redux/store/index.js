9360764294a33d210bd98eeead1f5f22bf992010
GiorgiaMannino <giorgimannino@hotmail.com>
2025-02-18 18:23:16 +0100

    Pratica S11-L1

--- Pratica S11 L2/src/redux/store/index.js
+++ Pratica S11 L2/src/redux/store/index.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import favouritesReducer from "../reducers/favouritesReducer"; // Importa il reducer per i preferiti
-import searchResultsReducer from "../reducers/searchResultsReducer"; // Importa il reducer per i risultati di ricerca
+import favouritesReducer from "../reducers/favouritesReducer";
+import searchResultsReducer from "../reducers/searchResultsReducer";
 
 const store = configureStore({
   reducer: {
-    favourites: favouritesReducer, // Aggiungi il reducer per i preferiti
-    searchResults: searchResultsReducer, // Aggiungi il reducer per i risultati di ricerca
+    favourites: favouritesReducer,
+    searchResults: searchResultsReducer,
   },
 });
 
